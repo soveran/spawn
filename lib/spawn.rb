ca47a374f8fc01c2cf57240797cc23e0cd81b5ca
Michel Martens <michel@soveran.com>
2009-07-04 20:29:57 -0300

    Experimental version of mass assignments

--- lib/spawn.rb
+++ lib/spawn.rb
@@ -1,14 +1,29 @@
 require "ostruct"
 
 module Spawn
+
+  Invalid = Class.new(ArgumentError)
+
   def spawner &default
     @@spawn ||= Hash.new
     @@spawn[self] = default
   end
 
-  def spawn attrs = {}
-    @@spawn[self].call(model = OpenStruct.new(attrs))
-    factory_method = respond_to?(:create!) ? :create! : :create
-    send(factory_method, model.send(:table).merge(attrs))
+  def spawn params = {}
+    model = new
+
+    # Grab default parameters from spawner block.
+    @@spawn[self].call(attrs = OpenStruct.new(params))
+
+    # Assign attributes to model.
+    attrs.send(:table).merge(params).each do |key, value|
+      model.send("#{key}=", value)
+    end
+
+    # Yield model for changes to be made before saving.
+    yield(model) if block_given?
+
+    # Return the model if it's valid and can be saved.
+    model.valid? and model.save or raise(Invalid)
   end
 end
--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -16,6 +16,7 @@ class ActiveRecordUser < ActiveRecord::Base
   extend Spawn
 
   validates_presence_of :name
+  attr_accessible :name
 
   spawner do |user|
     user.name = Faker::Name.name
@@ -24,21 +25,28 @@ class ActiveRecordUser < ActiveRecord::Base
 end
 
 class TestSpawnWithActiveRecord < Test::Unit::TestCase
-  setup do
-    @user = ActiveRecordUser.spawn :name => "John"
-  end
-
   context "spawned user" do
+    setup do
+      @user = ActiveRecordUser.spawn :name => "John"
+    end
+
     should "have John as name" do
       assert_equal "John", @user.name
     end
 
     context "with invalid attributes" do
       should "raise an error" do
-        assert_raise ActiveRecord::RecordInvalid do
+        assert_raise Spawn::Invalid do
           ActiveRecordUser.spawn :name => nil
         end
       end
+    end
+  end
+
+  context "working with attr_accessible" do
+    should "bypass that limitation" do
+      @user = ActiveRecordUser.spawn :email => "john@example.com"
+      assert_equal "john@example.com", @user.email
     end
   end
 end
